28fb53b494aca6607f17ef3270114a819c6f5f08
kdaniel1124 <kennellydanny@gmail.com>
2023-11-22 14:47:27 -0500

    Gravity Pull Kata solution tweaked to be alittle bit more concise. Additionally I wrote unit tests for the solution and all are passing.

---
+++ CodeWarsKatas/CodeWarsTests/GravityFlipTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass]
+    public class GravityFlipTests
+    {
+        [TestMethod]
+        public void GravityFlip_Flip_LeftGravity()
+        {
+            //Arrange
+            GravityFlip Test1 = new GravityFlip();
+            int[] inputArr = { 1, 3, 2, 5, 4, 4 };
+            char inputDir = 'L';
+            int[] expected = { 5, 4, 4, 3, 2, 1};
+
+            //Act
+            int[] result = Test1.Flip(inputDir, inputArr);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GravityFlip_Flip_RightGravity()
+        {
+            //Arrange
+            GravityFlip Test2 = new GravityFlip();
+            int[] inputArr = { 1, 3, 2, 10, 2, 5, 4, 4 };
+            char inputDir = 'R';
+            int[] expected = { 1, 2, 2, 3, 4, 4, 5, 10 };
+
+            //Act
+            int[] result = Test2.Flip(inputDir, inputArr);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GravityFlip_Flip_NoBlocks()
+        {
+            //Arrange
+            GravityFlip Test3 = new GravityFlip();
+            int[] inputArr = { };
+            char inputDir = 'L';
+            int[] expected = { };
+
+            //Act
+            int[] result = Test3.Flip(inputDir, inputArr);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+    }
+}
