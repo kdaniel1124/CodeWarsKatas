205cc8ffe19e1b32c462fe3d5701fd646d097863
kdaniel1124 <kennellydanny@gmail.com>
2023-11-22 14:31:52 -0500

    Gravity Flip kata solution simplified and complete, still needs to be tested

---
+++ CodeWarsKatas/CodeWarsKatas/GravityFlip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas
+{
+    public class GravityFlip
+    {
+        public int[] Flip(char dir, int[] arr)
+        {
+            List<int> stacks = arr.ToList();
+            stacks.Sort();
+
+            if (dir == 'L')
+            {
+                stacks.Reverse();
+            }
+
+            int[] sortedStacks = stacks.ToArray();
+
+            return sortedStacks;
+        }
+    }
+}
