e0b60993a591ca7bf93f92e312e8b32639b51afa
kdaniel1124 <kennellydanny@gmail.com>
2023-11-29 12:42:30 -0500

    SpoonerizeMe solution completed, still need to write tests.

---
+++ CodeWarsKatas/CodeWarsKatas/SpoonerizeMe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas
+{
+    public class SpoonerizeMe
+    {
+        public string Spoonerize(string str)
+        {
+            char first = str[0];
+            string result = "";
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i - 1] == ' ')
+                {
+                    result = str[i] + result + first;
+                }
+                else
+                {
+                    result += str[i];
+                }
+            }
+            return result;
+        }
+    }
+}
