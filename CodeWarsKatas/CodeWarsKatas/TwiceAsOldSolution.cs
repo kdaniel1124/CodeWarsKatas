c41b73541b2840a950f38b48b609bf2791684263
kdaniel1124 <kennellydanny@gmail.com>
2023-12-04 15:18:21 -0500

    TwiceAsOld kata solution completed, first iteration. TODO: unit tests

---
+++ CodeWarsKatas/CodeWarsKatas/TwiceAsOldSolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas
+{
+    public class TwiceAsOldSolution
+    {
+        public int TwiceAsOld(int dadYears, int sonYears)
+        {
+            int dadAge = dadYears - sonYears;
+            int sonAge = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                if( sonAge * 2 == dadAge)
+                {
+                    break;
+                }
+                else
+                {
+                    sonAge++;
+                    dadAge++;
+                }
+            }
+            return sonAge;
+        }
+    }
+}
