094f527354dcbf0f04d1d43ab7d4d840dc8bd42a
kdaniel1124 <kennellydanny@gmail.com>
2023-12-14 10:29:45 -0500

    Multiples of 3 or 5 kata first iteration completed. TODO: unit tests.

---
+++ CodeWarsKatas/CodeWarsKatas/MultiplesOfThreeOrFive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas
+{
+    public class MultiplesOfThreeOrFive
+    {
+        public int Solution(int value)
+        {
+            int result = 0;
+
+            for (int i = 0; i < value; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    result += i;
+                }
+                else if (i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+    }
+}
