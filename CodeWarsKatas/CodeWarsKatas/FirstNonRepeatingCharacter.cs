240146850124a76d0f9d85aa8469e6db15f9978f
kdaniel1124 <kennellydanny@gmail.com>
2023-12-15 12:18:05 -0500

    First Non Repeat kata commented and fully finished!

--- CodeWarsKatas/CodeWarsKatas/FirstNonRepeatingCharacter.cs
+++ CodeWarsKatas/CodeWarsKatas/FirstNonRepeatingCharacter.cs
@@ -6,22 +6,29 @@ using System.Threading.Tasks;
 
 namespace CodeWarsKatas
 {
+    //https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
     public class FirstNonRepeatingCharacter
     {
         public string FirstNonRepeat(string str)
         {
+            // We make a lowercase version of our input string to loop through so we can be case insensative
             string lowerString = str.ToLower();
 
             for (int i = 0; i < lowerString.Length; i++)
             {
+                // Stored each char in a variable call currentLetter just for readability
                 char currentLetter = lowerString[i];
 
+                // With this conditional we compare the (first)IndexOf and LastIndexOf the current letter
+                // If first and last occurence are different indexes then we know it only occurs more than once
                 if (lowerString.IndexOf(currentLetter) == lowerString.LastIndexOf(currentLetter))
                 {
+                    // We return the index from the original input as opposed to our lower case string
                     return str[i].ToString();
                 }
             }
 
+            // If we loop throught the whole string with only repeat characters then we are to simply return an empty string
             return "";
         }
     }
