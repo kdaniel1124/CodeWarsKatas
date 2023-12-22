f58293877087a489998df3fdcc36dbbe42507659
kdaniel1124 <kennellydanny@gmail.com>
2023-12-22 18:02:47 -0500

    Candy Problem kata most tests completed. TODO: write a couple more tests for a few more edge cases

---
+++ CodeWarsKatas/CodeWarsTests/CandyProblemTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass]
+    public class CandyProblemTests
+    {
+        [TestMethod]
+        public void CandyProblem_GetMissingCandies_AlreadyBalanced()
+        {
+            //Arrange
+            CandyProblem Test1 = new CandyProblem();
+            int[] input = { 10, 10, 10, 10};
+            int expected = 0;
+
+            //Act
+            int result = Test1.GetMissingCandies(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "If all candy amounts are already the same, then there is no need to add any extra.");
+
+        }
+    }
+}
