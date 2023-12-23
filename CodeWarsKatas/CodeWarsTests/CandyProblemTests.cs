5d88abbd9b7c5fc348d448e04d049aff351a4226
kdaniel1124 <kennellydanny@gmail.com>
2023-12-23 17:04:40 -0500

    Candy Problem kata, all unit tests complete.

--- CodeWarsKatas/CodeWarsTests/CandyProblemTests.cs
+++ CodeWarsKatas/CodeWarsTests/CandyProblemTests.cs
@@ -23,7 +23,37 @@ namespace CodeWarsTests
 
             //Assert
             Assert.AreEqual(expected, result, "If all candy amounts are already the same, then there is no need to add any extra.");
-
         }
+
+        [TestMethod]
+        public void CandyProblem_GetMissingCandies_EmptyArray()
+        {
+            //Arrange
+            CandyProblem Test1 = new CandyProblem();
+            int[] input = { };
+            int expected = -1;
+
+            //Act
+            int result = Test1.GetMissingCandies(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "If provided an array of length 1 or less, the method should return -1.");
+        }
+
+        [TestMethod]
+        public void CandyProblem_GetMissingCandies_LargeGap()
+        {
+            //Arrange
+            CandyProblem Test1 = new CandyProblem();
+            int[] input = { 0, 20, 1, 1};
+            int expected = 58;
+
+            //Act
+            int result = Test1.GetMissingCandies(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should return the number of candies needed to bring all kids up to the highest amount in the array.");
+        }
+
     }
 }
