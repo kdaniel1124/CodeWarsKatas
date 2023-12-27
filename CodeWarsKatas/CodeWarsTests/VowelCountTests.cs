3f7402f0bbbc101f25eb42c059bd8fb7f6bfdb98
kdaniel1124 <kennellydanny@gmail.com>
2023-12-27 13:44:58 -0500

    Vowel Count tests set up, body needs finished

---
+++ CodeWarsKatas/CodeWarsTests/VowelCountTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass]
+    public class VowelCountTests
+    {
+        [TestMethod]
+        public void VowelCount_GetVowelCount_VowelsAndSpaces()
+        {
+
+        }
+    }
+}
