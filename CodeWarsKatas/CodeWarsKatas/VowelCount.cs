45fc30d1604394ffddda062882947d87955e98a4
kdaniel1124 <kennellydanny@gmail.com>
2023-12-28 11:47:15 -0500

    Vowel Count kata completed and refactored and all unit tests are written and passing.

---
+++ CodeWarsKatas/CodeWarsKatas/VowelCount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas
+{
+    public class VowelCount
+    {
+        public int GetVowelCount(string str)
+        {
+            string[] vowelBank = { "a", "e", "i", "o", "u" };
+
+            int vowelCount = 0;
+
+            foreach(char letter in str)
+            {
+                if (vowelBank.Contains(letter.ToString().ToLower()))
+                {
+                    vowelCount++;
+                }
+            }
+
+            return vowelCount;
+        }
+    }
+}
--- CodeWarsKatas/CodeWarsTests/VowelCountTests.cs
+++ CodeWarsKatas/CodeWarsTests/VowelCountTests.cs
@@ -13,7 +13,47 @@ namespace CodeWarsTests
         [TestMethod]
         public void VowelCount_GetVowelCount_VowelsAndSpaces()
         {
+            //Arrange
+            VowelCount Test1 = new VowelCount();
+            string input = "aa i oo i uu";
+            int expected = 8;
 
+            //Act
+            int result = Test1.GetVowelCount(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should return the amount of vowels in the input string.");
         }
+
+        [TestMethod]
+        public void VowelCount_GetVowelCount_CaseSensitiveVowels()
+        {
+            //Arrange
+            VowelCount Test2 = new VowelCount();
+            string input = "Are you coming?";
+            int expected = 6;
+
+            //Act
+            int result = Test2.GetVowelCount(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "Method should return the amount of vowels in the input string regardless of their casing.");
+        }
+
+        [TestMethod]
+        public void VowelCount_GetVowelCount_EmptyString()
+        {
+            //Arrange
+            VowelCount Test3 = new VowelCount();
+            string input = "";
+            int expected = 0;
+
+            //Act
+            int result = Test3.GetVowelCount(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "If provided an empty string, method should simply return zero.");
+        }
+
     }
 }
