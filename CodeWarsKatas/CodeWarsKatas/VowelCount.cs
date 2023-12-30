a69c8810731e2c89b65c02cc86d94455dfb4feca
kdaniel1124 <kennellydanny@gmail.com>
2023-12-29 23:12:09 -0500

    fixed issue where the link to several katas wasn't included

--- CodeWarsKatas/CodeWarsKatas/VowelCount.cs
+++ CodeWarsKatas/CodeWarsKatas/VowelCount.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace CodeWarsKatas
 {
+    // https://www.codewars.com/kata/54ff3102c1bad923760001f3
     public class VowelCount
     {
         public int GetVowelCount(string str)
