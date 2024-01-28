9acdc81afe7f666d152b7afd8e27beb9b966fb91
kdaniel1124 <kennellydanny@gmail.com>
2024-01-27 22:39:10 -0500

    CloneWars kata solution completed

---
+++ CodeWarsKatas/CodeWarsKatas/SendInTheClones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas
+{
+    public class SendInTheClones
+    {
+        public int[] CloneWars(int kataPerDay)
+        {
+            int cloneCount = 1;
+            int kataCompleted = 0;
+
+            for (int i = kataPerDay; i > 0; i--)
+            {
+                kataCompleted += i * cloneCount;
+                cloneCount *= 2;
+            }
+
+            int[] result = new int[] { cloneCount, kataCompleted };
+
+            return result;
+        }
+    }
+}
--- CodeWarsKatas/CodeWarsKatas/obj/Debug/net6.0/CodeWarsKatas.AssemblyInfo.cs
+++ CodeWarsKatas/CodeWarsKatas/obj/Debug/net6.0/CodeWarsKatas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeWarsKatas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2741a7b49a15a27515d9c8e6528ae5965e1490f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a95f595bf9160621ce1a802a9714c8ad1f530f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeWarsKatas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeWarsKatas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
