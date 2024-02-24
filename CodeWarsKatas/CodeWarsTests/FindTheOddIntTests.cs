343a9bc8d5f238ba636e02fb5b497fad4efb5d63
kdaniel1124 <kennellydanny@gmail.com>
2024-02-23 22:17:18 -0500

    Fint the odd int initial tests complete. TODO: remember to comment in the url to the kata on codewars, and write a few more tests

---
+++ CodeWarsKatas/CodeWarsTests/FindTheOddIntTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass]
+    public class FindTheOddIntTests
+    {
+        [TestMethod]
+        public void FindTheOddInt_FindOdd_OnlyOdd()
+        {
+            //Arrange
+            FindTheOddInt Test1 = new FindTheOddInt();
+            int[] input = { 15 };
+            int expected = 15;
+
+            //Act
+            int result = Test1.FindOdd(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "");
+        }
+    }
+}
--- CodeWarsKatas/CodeWarsTests/obj/Debug/net6.0/CodeWarsTests.AssemblyInfo.cs
+++ CodeWarsKatas/CodeWarsTests/obj/Debug/net6.0/CodeWarsTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeWarsTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485674063e201abc271da1a528b50c46dd6b3b87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8bed79e91e5725b0576df25646635c41f36121")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeWarsTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeWarsTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
