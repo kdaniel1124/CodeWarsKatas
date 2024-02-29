f484ba6ead90c5be787669a7c5c7a88d69689b9c
kdaniel1124 <kennellydanny@gmail.com>
2024-02-29 14:26:08 -0500

    Find the odd kata tests written. TODO: Include link to original kata in comments, also seek clarification as it pertains having no odds or an empty array.

--- CodeWarsKatas/CodeWarsTests/FindTheOddIntTests.cs
+++ CodeWarsKatas/CodeWarsTests/FindTheOddIntTests.cs
@@ -24,5 +24,36 @@ namespace CodeWarsTests
             //Assert
             Assert.AreEqual(expected, result, "");
         }
+
+        [TestMethod]
+        public void FindTheOddInt_FindOdd_OnlyEvens()
+        {
+            //Arrange
+            FindTheOddInt Test2 = new FindTheOddInt();
+            int[] input = { 15, 30, 45, 60, 60, 45, 30, 15 };
+            int expected = 15;
+
+            //Act
+            int result = Test2.FindOdd(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "");
+        }
+
+        [TestMethod]
+        public void FindTheOddInt_FindOdd_OneOddAtEnd()
+        {
+            //Arrange
+            FindTheOddInt Test3 = new FindTheOddInt();
+            int[] input = { 1, 2, 3, 1, 2, 3, 4 };
+            int expected = 4;
+
+            //Act
+            int result = Test3.FindOdd(input);
+
+            //Assert
+            Assert.AreEqual(expected, result, "");
+        }
+
     }
 }
